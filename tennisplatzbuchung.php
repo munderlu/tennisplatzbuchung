655963bfc5b3fb2bd1faa4634add709ae244fc24
Luke Munderich <luke.munderich@web.de>
2022-07-21 15:52:37 +0200

    Die Daten der nächsten drei Tage werden in einer Tabelle angezeigt.

--- tennisplatzbuchung.php
+++ tennisplatzbuchung.php
@@ -12,7 +12,7 @@ $timestamp=time();
 $heutigesDatum=date("d.m", $timestamp);
 $geteiltesHeutigesDatum=explode(".", $heutigesDatum);
 $dbh=new PDO("mysql: host=localhost; dbname=tennisplatzbuchung", "luke", "Fallen2211");
-if(empty($_REQUEST["passwort"]) && empty($_REQUEST["datum"])){
+if(empty($_REQUEST["passwort"]) && empty($_REQUEST["datum"]) && empty($_REQUEST["buchungsdatum"])){
     print '<h1>Bitte geben Sie das Passwort ein:</h1>
         <form method="post" action="tennisplatzbuchung.php">
             <input type="password" name="passwort">
@@ -49,15 +49,49 @@ if(!empty($_REQUEST["passwort"])){
     $ergebnis=$dbh->query($sql);
     $rueckgabewert=$ergebnis->fetchAll(PDO::FETCH_ASSOC);
     if($_REQUEST["passwort"]==$rueckgabewert[0]["i_inhalt"]){
-        for($i=0; $i<7; $i++){
-            $daten=array();
+        for($i=0; $i<3; $i++){
             $timestamp=time()+60*60*24*$i;
             $datum=date("d.m", $timestamp);
-            $sql2="SELECT * FROM daten WHERE d_datum='".$datum."' AND d_uhrzeit='7:00';";
+            $sql2="SELECT * FROM daten WHERE d_datum='".$datum."';";
             $ergebnis=$dbh->query($sql2);
             $daten[$i]=$ergebnis->fetchAll(PDO::FETCH_ASSOC);
-            print $daten[$i][0]["d_platz1"]." ".$daten[$i][0]["d_datum"]."<br>";
+            //print $daten[$i][0]["d_platz1"]." ".$daten[$i][0]["d_datum"]."<br>";
         }
+        print '<table>
+                    <tr>
+                        <th>Datum</th>
+                        <th>Uhrzeit</th>
+                        <th>Platz1</th>
+                        <th>Platz2</th>
+                    </tr>';
+        foreach($daten as $i){
+            foreach($i as $j){
+                if($j["d_platz1"]==0){
+                    $statusPlatz1="frei";
+                }
+                else{
+                    $statusPlatz1="belegt";
+                }
+                if($j["d_platz2"]==0){
+                    $statusPlatz2="frei";
+                }
+                else{
+                    $statusPlatz2="belegt";
+                }
+                print '<tr><td>'.$j["d_datum"].'</td>';
+                print '<td>'.$j["d_uhrzeit"].'</td>';
+                print '<td><form method="post" action="tennisplatzbuchung.php">
+                    <input type="hidden" value="'.$j["d_datum"].'" name="buchungsdatum">
+                    <input type="hidden" value="'.$statusPlatz1.'" name="statusPlatz">
+                    <input type="submit" value="'.$statusPlatz1.'"></form></td>';
+                print '<td><form method="post" action="tennisplatzbuchung.php">
+                    <input type="hidden" value="'.$j["d_datum"].'" name="buchungsdatum">
+                    <input type="hidden" value="'.$statusPlatz2.'" name="statusPlatz">
+                    <input type="submit" value="'.$statusPlatz2.'"></form></td></tr>';
+            }
+        }
+        print "</table>";
+        //jetzt kommt die Abfrage für einen anderen Tag
         print '<h1>Möchten Sie an einem anderen Tag buchen?</h1>
             <form method="post" action="tennisplatzbuchung.php">
                 <input type="text" name="datum" placeholder="TT.MM">
@@ -65,7 +99,6 @@ if(!empty($_REQUEST["passwort"])){
             </form>';
         print "Heute ist der ".$heutigesDatum;
         print "<br>$geteiltesHeutigesDatum[0]<br>$geteiltesHeutigesDatum[1]";
-        //hier muss jetzt die Tabele mit den Plätzen der nächsten sieben Tagen stehen
     }
     else{
         print "Falsches Passwort. <form method='post' action='tennisplatzbuchung.php'><input type='submit' value='Weiter'></form>";
