b21b92c2f296362338d9b3b5280759c3944e227c
Luke Munderich <luke.munderich@web.de>
2022-08-02 12:19:18 +0200

    Sicherheit gewährleistet und Aussehen der Tabellen angepasst.

--- tennisplatzbuchung.php
+++ tennisplatzbuchung.php
@@ -24,81 +24,80 @@ if((empty($_REQUEST["passwort"]) || empty($_REQUEST["benutzername"])) && empty($
 if(!empty($_REQUEST["passwort"]) && !empty($_REQUEST["benutzername"])){
     $passwort=$_REQUEST["passwort"];
     $benutzername=$_REQUEST["benutzername"];
-    $sql="SELECT * FROM konten WHERE k_benutzername='$benutzername';";
+    $richtigeEingabe=False;
+    $sql="SELECT * FROM konten;";
     $ergebnis=$dbh->query($sql);
-    if($ergebnis->rowCount()>0){
-        $rueckgabewert=$ergebnis->fetchAll(PDO::FETCH_ASSOC);
-        $richtigesPasswort=$rueckgabewert[0]["k_passwort"];
-    }
-    else{
-        $richtigesPasswort=0;
-    }
-    if($passwort==$richtigesPasswort && $ergebnis->rowCount()>0){
-        for($i=0; $i<3; $i++){
-            $timestamp=time()+60*60*24*$i;
-            $datum=date("d.m.", $timestamp);
-            $sql2="SELECT * FROM daten WHERE d_datum='".$datum."';";
-            $ergebnis=$dbh->query($sql2);
-            $daten[$i]=$ergebnis->fetchAll(PDO::FETCH_ASSOC);
-        }
-        foreach($daten as $i){
-            print '<table>
-                        <tr>
-                            <th>Datum</th>
-                            <th>Uhrzeit</th>
-                            <th>Platz1</th>
-                            <th>Platz2</th>
-                        </tr>';
-            foreach($i as $j){
-                if($j["d_platz1"]==0){
-                    $statusPlatz1="Frei";
-                    $farbe1="green";
-                }
-                else{
-                    $statusPlatz1=$j["d_bucherplatz1"]."";
-                    $farbe1="red";
-                }
-                if($j["d_platz2"]==0){
-                    $statusPlatz2="Frei";
-                    $farbe2="green";
-                }
-                else{
-                    $statusPlatz2=$j["d_bucherplatz2"]."";
-                    $farbe2="red";
-                }
-                print '<tr><td>'.$j["d_datum"].'</td>';
-                print '<td>'.$j["d_uhrzeit"].'</td>';
-                print '<td style="background-color:'.$farbe1.'"><form method="post" action="tennisplatzbuchung.php">
-                    <input type="hidden" value="'.$j["d_datum"].'" name="buchungsdatum">
-                    <input type="hidden" value="'.$j["d_uhrzeit"].'" name="uhrzeit">
-                    <input type="hidden" value="'.$statusPlatz1.'" name="statusPlatz">
-                    <input type="hidden" value="d_platz1" name="platz">
-                    <input type="hidden" value="'.$benutzername.'" name="benutzer">
-                    <input type="hidden" value="'.$passwort.'" name="passwort">
-                    <input type="submit" value="'.$statusPlatz1.'"></form></td>';
-                print '<td style="background-color:'.$farbe2.'"><form method="post" action="tennisplatzbuchung.php">
-                    <input type="hidden" value="'.$j["d_datum"].'" name="buchungsdatum">
-                    <input type="hidden" value="'.$j["d_uhrzeit"].'" name="uhrzeit">
-                    <input type="hidden" value="'.$statusPlatz2.'" name="statusPlatz">
-                    <input type="hidden" value="d_platz2" name="platz">
-                    <input type="hidden" value="'.$benutzername.'" name="benutzer">
-                    <input type="hidden" value="'.$passwort.'" name="passwort">
-                    <input type="submit" value="'.$statusPlatz2.'"></form></td></tr>';
+    $rueckgabewert=$ergebnis->fetchAll(PDO::FETCH_ASSOC);
+    foreach($rueckgabewert as $wert){
+        if($wert["k_benutzername"]==$benutzername && $wert["k_passwort"]==$passwort){
+            for($i=0; $i<3; $i++){
+                $timestamp=time()+60*60*24*$i;
+                $datum=date("d.m.", $timestamp);
+                $sql2="SELECT * FROM daten WHERE d_datum='".$datum."';";
+                $ergebnis=$dbh->query($sql2);
+                $daten[$i]=$ergebnis->fetchAll(PDO::FETCH_ASSOC);
             }
-            print "</table>";
+            foreach($daten as $i){
+                print '<table>
+                            <tr><th colspan="3">'.$i[0]["d_datum"].'</th></tr>
+                            <tr>
+                                <th>Uhrzeit</th>
+                                <th>Platz1</th>
+                                <th>Platz2</th>
+                            </tr>';
+                foreach($i as $j){
+                    if($j["d_platz1"]==0){
+                        $statusPlatz1="Frei";
+                        $farbe1="green";
+                    }
+                    else{
+                        $statusPlatz1=$j["d_bucherplatz1"]."";
+                        $farbe1="red";
+                    }
+                    if($j["d_platz2"]==0){
+                        $statusPlatz2="Frei";
+                        $farbe2="green";
+                    }
+                    else{
+                        $statusPlatz2=$j["d_bucherplatz2"]."";
+                        $farbe2="red";
+                    }
+                    print '<tr>';
+                    print '<td style="background-color: orange">'.$j["d_uhrzeit"].'</td>';
+                    print '<td style="background-color:'.$farbe1.'"><form method="post" action="tennisplatzbuchung.php">
+                        <input type="hidden" value="'.$j["d_datum"].'" name="buchungsdatum">
+                        <input type="hidden" value="'.$j["d_uhrzeit"].'" name="uhrzeit">
+                        <input type="hidden" value="'.$statusPlatz1.'" name="statusPlatz">
+                        <input type="hidden" value="d_platz1" name="platz">
+                        <input type="hidden" value="'.$benutzername.'" name="benutzer">
+                        <input type="hidden" value="'.$passwort.'" name="passwort">
+                        <input type="submit" value="'.$statusPlatz1.'"></form></td>';
+                    print '<td style="background-color:'.$farbe2.'"><form method="post" action="tennisplatzbuchung.php">
+                        <input type="hidden" value="'.$j["d_datum"].'" name="buchungsdatum">
+                        <input type="hidden" value="'.$j["d_uhrzeit"].'" name="uhrzeit">
+                        <input type="hidden" value="'.$statusPlatz2.'" name="statusPlatz">
+                        <input type="hidden" value="d_platz2" name="platz">
+                        <input type="hidden" value="'.$benutzername.'" name="benutzer">
+                        <input type="hidden" value="'.$passwort.'" name="passwort">
+                        <input type="submit" value="'.$statusPlatz2.'"></form></td></tr>';
+                }
+                print "</table>";
+            }
+            //jetzt kommt die Abfrage für einen anderen Tag
+            print '<h1>Möchten Sie an einem anderen Tag buchen?</h1>
+                <form method="post" action="tennisplatzbuchung.php">
+                    <input type="date" name="datum">
+                    <input type="hidden" value="'.$benutzername.'" name="benutzer">
+                    <input type="hidden" value="'.$passwort.'" name="passwort">
+                    <input type="submit" value="Weiter">
+                </form>';
+            print '<br><form action="tennisplatzbuchung.php" method="post">
+                <input type="submit" value="Abmelden"></form>';
+            $richtigeEingabe=True;
+            break;
         }
-        //jetzt kommt die Abfrage für einen anderen Tag
-        print '<h1>Möchten Sie an einem anderen Tag buchen?</h1>
-            <form method="post" action="tennisplatzbuchung.php">
-                <input type="date" name="datum">
-                <input type="hidden" value="'.$benutzername.'" name="benutzer">
-                <input type="hidden" value="'.$passwort.'" name="passwort">
-                <input type="submit" value="Weiter">
-            </form>';
-        print '<br><form action="tennisplatzbuchung.php" method="post">
-            <input type="submit" value="Abmelden"></form>';
     }
-    else{
+    if(!$richtigeEingabe){
         print "<h1>Sie haben ein falsches Passwort oder einen falschen Benutzername eingegeben.</h1>
             <form method='post' action='tennisplatzbuchung.php'><input type='submit' value='Weiter'></form>";
     }
@@ -122,8 +121,8 @@ if(!empty($_REQUEST["datum"])){
     $idInZweiWochen=$ergebnis[0]["d_id"];
     if($id<$idInZweiWochen){
         print '<table>
+                    <tr><th colspan="3">'.$tag.'.'.$monat.'</th></tr>
                     <tr>
-                        <th>Datum</th>
                         <th>Uhrzeit</th>
                         <th>Platz1</th>
                         <th>Platz2</th>
@@ -145,8 +144,8 @@ if(!empty($_REQUEST["datum"])){
                 $statusPlatz2=$i["d_bucherplatz2"]."";
                 $farbe2="red";
             }
-            print '<tr><td>'.$i["d_datum"].'</td>';
-            print '<td>'.$i["d_uhrzeit"].'</td>';
+            print '<tr>';
+            print '<td style="background-color: orange">'.$i["d_uhrzeit"].'</td>';
             print '<td style="background-color:'.$farbe1.'"><form method="post" action="tennisplatzbuchung.php">
                 <input type="hidden" value="'.$i["d_datum"].'" name="buchungsdatum">
                 <input type="hidden" value="'.$i["d_uhrzeit"].'" name="uhrzeit">
