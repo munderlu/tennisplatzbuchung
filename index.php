cac4a0944cb1cddac8438b94f3f68d2f01577c72
SURFACE-LM <luke.munderich@web.de>
2022-11-19 14:30:19 +0100

    Verbessertes Design

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="Logo.png">
-    <link rel="manifest" href="manifest.webmanifest">
+    <link rel="manifest" href="/manifest.webmanifest">
     <title>Tennisplatz Schopfloch</title>
 </head>
 <body>
